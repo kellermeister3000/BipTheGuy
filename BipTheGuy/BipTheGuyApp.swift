a147f77b6c36bec7a6e05db74ecf411bfedb210e
Philip Keller <philipjoelkeller@gmail.com>
2023-02-15 13:33:02 -0800

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Philip Keller on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
